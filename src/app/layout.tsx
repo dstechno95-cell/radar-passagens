97ed126fb7ddfc89cd4720f7a29fb91d3f7acf0f
Daniel <dstechno95@gmail.com>
2026-05-18 20:57:41 -0300

    feat: impact.com tracking script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -24,7 +25,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
         <meta name="impact-site-verification" {...{ value: '4edeef75-288f-4c94-9727-f5e836577389' } as any} />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          id="impact-tracking"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]||function(){(c[p].a=c[p].a||[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A7320323-accf-4d88-947e-c864d76086041.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');`,
+          }}
+        />
+      </body>
     </html>
   );
 }
